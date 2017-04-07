521f685b67ad7e64b7ba21cc276c3f186c9ee10b
Russelsp <steven.russell@alumni.emory.edu>
2017-04-07 12:44:21 -0400

    1st Edits post Curtis's feedback

--- Graphs for Publication.R
+++ Graphs for Publication.R
@@ -1,9 +1,6 @@
 
-his.combined <- read.csv("//cdc.gov/private/L330/ykf1/New folder/Data Quality/Paper/his.combined.csv")
-
-# Next time use stringsAsFactors = F in read.csv
-
-
+his.combined <- read.csv("//cdc.gov/private/L330/ykf1/New folder/Data Quality/Paper/his.combined.csv",
+                         stringsAsFactors = F)
 
 # Loading packages
 
@@ -46,13 +43,113 @@ EWARN.somaliland <- read.csv("//cdc.gov/private/L330/ykf1/New folder/Data Qualit
 EWARN.southern <- read.csv("//cdc.gov/private/L330/ykf1/New folder/Data Quality/Paper/southern2013.csv", 
                            stringsAsFactors = F, na.strings = ".")
 
+#---------------------------------------------------------------------------------------------#
+
+# Figure 1A (HIS, female 18-59, AMR)
+
+final.digit = 0:9
+Freq = c(15, 0, 0, 2, 2, 5, 1, 3, 4, 3)
+t3 <- data.frame(cbind(final.digit, Freq))
+t3$final.digit <- as.factor(t3$final.digit)
+
+p1 <- ggplot(t3, aes(x=final.digit, y=Freq)) + 
+  geom_bar(stat="identity") 
+
+plot1 <- p1 +
+  geom_bar(stat="identity", fill="grey16") +
+  xlab("Final Digit (female 18-59, other conditions)") +
+  ylab("Count") +
+  ggtitle("Occurances of Final Digit (HIS)") +
+  theme_base() +
+  theme(plot.title = element_text(size = 12, face = "bold"))+
+  theme(axis.title.x = element_text(size = 11),
+        axis.title.y = element_text(size = 11)) +
+  scale_y_continuous(breaks = seq(5, 20, 5)) + 
+  expand_limits(y=21) +
+  annotate("text", x=2, y=20, label = "p < .0001", size=5)
+
+# Figure 1B (EWARN)
+
+final.digit = 0:9
+Freq = c(14, 5, 2, 5, 1, 10, 2, 5, 4, 3)
+t1 <- data.frame(cbind(final.digit, Freq))
+t1$final.digit <- as.factor(t1$final.digit)
+
+p2 <- ggplot(t1, aes(x=final.digit, y=Freq)) + 
+  geom_bar(stat="identity") 
+
+plot2 <- p2 +
+  geom_bar(stat="identity", fill="grey16") +
+  xlab("Final Digit (5 and over, other cases)") +
+  ylab("Count") +
+  ggtitle("Occurances of Final Digit (EWARN)") +
+  theme_base() +
+  theme(plot.title = element_text(size = 12, face = "bold"))+
+  theme(axis.title.x = element_text(size = 11),
+        axis.title.y = element_text(size = 11)) +
+  scale_y_continuous(breaks = seq(5, 20, 5)) + 
+  expand_limits(y=21) +
+  annotate("text", x=2, y=20, label = "p = .0008", size=5)
+
+# Figure 1C (HIS, male 18-59, AMR)
+
+final.digit = 0:9
+Freq = c(12, 1, 2, 2, 3, 2, 3, 4, 2, 4)
+t4 <- data.frame(cbind(final.digit, Freq))
+t4$final.digit <- as.factor(t4$final.digit)
+
+p3 <- ggplot(t4, aes(x=final.digit, y=Freq)) + 
+  geom_bar(stat="identity") 
+
+plot3 <- p3 +
+  geom_bar(stat="identity", fill="grey16") +
+  xlab("Final Digit (male 18-59, other conditions)") +
+  ylab("Count") +
+  ggtitle("Occurances of Final Digit (HIS)") +
+  theme_base() +
+  theme(plot.title = element_text(size = 12, face = "bold")) +
+  theme(axis.title.x = element_text(size = 11),
+        axis.title.y = element_text(size = 11)) +
+  scale_y_continuous(breaks = seq(5, 20, 5)) + 
+  expand_limits(y=21) +
+  annotate("text", x=2, y=20, label = "p = .0027", size=5)
+
+# Figure 1D (EWARN)
+
+final.digit = 0:9
+Freq = c(1, 4, 5, 8, 0, 1, 4, 1, 5, 21)
+t2 <- data.frame(cbind(final.digit, Freq))
+t2$final.digit <- as.factor(t2$final.digit)
+
+p4 <- ggplot(t2, aes(x=final.digit, y=Freq)) + 
+  geom_bar(stat="identity") 
+
+plot4 <- p4 +
+  geom_bar(stat="identity", fill="grey16") +
+  xlab("Final Digit (under 5, other cases)") +
+  ylab("Count") +
+  ggtitle("Occurances of Final Digit (EWARN)") +
+  theme_base() +
+  theme(plot.title = element_text(size = 12, face = "bold"))+
+  theme(axis.title.x = element_text(size = 11),
+        axis.title.y = element_text(size = 11)) +
+  scale_y_continuous(breaks = seq(5, 20, 5)) + 
+  expand_limits(y=21) +
+  annotate("text", x=2, y=20, label = "p < .0001", size=5)
 
 
+# Saving Figure 1
 
+png('C:/Data-Quality/Figure1.png', 8, 6.5, units='in', res=300)
 
-# Plots
+multiplot(plot1, plot3, plot2, plot4, cols=2)
 
-# Creating Plot #1
+dev.off()
+
+#---------------------------------------------------------------------------------------------#
+
+# Figure 4A
+
 p1 <- ggplot( data = his.combined[his.combined$clinic=="JHAS",], 
               aes( x= week, y = count_M_over60_other)) 
 plot1 <- p1 + 
@@ -64,19 +161,7 @@ plot1 <- p1 +
     scale_x_continuous(breaks=c(10, 20, 30, 40, 50), 
                      labels = c("10", "20", "30", "40", "50"))
 
-   
-
-#p2 <- ggplot( data = his.combined[his.combined$clinic=="RAF",],
-#              aes( x= week, y = count_M_under5_ili)) 
-
-#plot2 <-  p2 +
-#  geom_line(size=1) + 
-#  xlab("Epidemiological Week") + 
-#  ylab("Cases (male under 5, ili conditions)") + 
-#  ggtitle("Cases by Week") +
-#  theme_gdocs() + 
-#  scale_x_continuous(breaks=c(10, 20, 30, 40, 50), 
-#                     labels = c("10", "20", "30", "40", "50"))      
+# Figure 4B
 
 p2 <- ggplot( data = his.combined[his.combined$clinic=="RAF",],
               aes( x= week, y = count_M_18_59_dental_cond))
@@ -90,17 +175,7 @@ plot2 <- p2 +
   scale_x_continuous(breaks=c(10, 20, 30, 40, 50), 
                      labels = c("10", "20", "30", "40", "50"))  
 
-#p4 <- ggplot( data = his.combined[his.combined$clinic=="RAF",],
-#              aes( x= week, y = count_F_18_59_dental_cond))
-
-#plot4 <- p4 + 
-#  geom_line(size=1) + 
-#  xlab("Epidemiological Week") + 
-#  ylab("Cases (female 18-59, dental conditions)") + 
-  ggtitle("Cases by Week") +
-  theme_gdocs() + 
-  scale_x_continuous(breaks=c(10, 20, 30, 40, 50), 
-                     labels = c("10", "20", "30", "40", "50")) 
+# Figure 4C
 
 p3 <- ggplot( data = EWARN.central[EWARN.central$facility=="Banadir Hospital",],
                 aes( x= Week, y = shigcasesless5))
@@ -114,7 +189,8 @@ plot3 <- p3 +
     scale_x_continuous(breaks=c(10, 20, 30, 40, 50), 
                        labels = c("10", "20", "30", "40", "50"))    
 
-  
+# Figure 4D
+
 p4 <- ggplot( data = EWARN.central[EWARN.central$facility=="Banadir Hospital",], 
               aes( x= Week, y = wccasesless5)) 
 
@@ -127,16 +203,17 @@ plot4 <- p4 +
   scale_x_continuous(breaks=c(10, 20, 30, 40, 50), 
                      labels = c("10", "20", "30", "40", "50")) 
 
-png('//cdc.gov/private/L330/ykf1/New folder/Data Quality/Paper/Figure4.png', 8, 6.5, units='in', res=300)
+# Saving Figure 4
 
-multiplot(plot1, plot2, plot4, plot3, cols=2)
+png('C:/Data-Quality/Figure4.png', 8, 6.5, units='in', res=300)
+
+multiplot(plot3, plot4, plot1, plot2, cols=2)
 
 dev.off()
 
-#http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
-#http://stackoverflow.com/questions/14323294/how-to-improve-jagged-line-graph-in-ggplot2 
-#http://blog.yhat.com/posts/replicating-five-thirty-eight-in-r.html
+#---------------------------------------------------------------------------------------------#
 
+# Figure 5A
 
 p5 <- ggplot( data = his.combined[his.combined$clinic=="MFH",], 
               aes( x= week, y = count_F_5_17_intestinal_worms)) 
@@ -153,6 +230,8 @@ plot5 <- p5 +
   #annotate("text", x=26, y=24, label= "Outlier: week 19", size=5)
 
 
+# Figure 5B
+
 p6 <- ggplot( data = his.combined[his.combined$clinic=="JHAS",], 
               aes( x= week, y = count_M_18_59_ear_infection)) 
 
@@ -167,7 +246,7 @@ plot6 <- p6 +
                      labels = c("10", "20", "30", "40", "50")) #+
   #annotate("text", x=31, y=34, label= "Outlier: week 23", size=5)
 
-
+# Figure 5C
 
 p7 <- ggplot( data = EWARN.puntland[EWARN.puntland$facility=="Bossaso MCH U/Y",], 
               aes( x= Week, y = malcases5over)) 
@@ -183,6 +262,8 @@ plot7 <- p7 +
                      labels = c("10", "20", "30", "40", "50")) #+
   #annotate("text", x=18, y=81, label= "Outlier: week 12", size=5)
 
+# Figure 5D
+
 p8 <- ggplot( data = EWARN.puntland[EWARN.puntland$facility=="Rakko MCH",], 
               aes( x= Week, y = cholcase5over)) 
 
@@ -196,105 +277,15 @@ plot8 <- p8 +
                      labels = c("10", "20", "30", "40", "50")) #+
   #annotate("text", x=15, y=7.8, label= "Outlier: week 8", size=5)
 
-png('//cdc.gov/private/L330/ykf1/New folder/Data Quality/Paper/Figure5.png', 8, 6.5, units='in', res=300)
+# Saving Figure 5
+
+png('C:/Data-Quality/Figure5.png', 8, 6.5, units='in', res=300)
 
 multiplot(plot5, plot6, plot7, plot8, cols=2)
 
 dev.off()
 
-# digit preference
 
-# plot 9
-
-final.digit = 0:9
-Freq = c(14, 5, 2, 5, 1, 10, 2, 5, 4, 3)
-t1 <- data.frame(cbind(final.digit, Freq))
-t1$final.digit <- as.factor(t1$final.digit)
-
-p9 <- ggplot(t1, aes(x=final.digit, y=Freq)) + 
-      geom_bar(stat="identity") 
-
-plot9 <- p9 +
-  geom_bar(stat="identity", fill="grey16") +
-  xlab("Final Digit (5 and over, other cases)") +
-  ylab("Frequency") +
-  ggtitle("Occurances of Final Digit (EWARN)") +
-  theme_base() +
-  theme(plot.title = element_text(size = 12, face = "bold"))+
-  theme(axis.title.x = element_text(size = 11),
-        axis.title.y = element_text(size = 11))
-
-# plot 10
-
-final.digit1 <- data.frame(EWARN.somaliland[EWARN.somaliland$fcode=="SLTO07",]$occasesless5)
-names(final.digit1) <- "count"
-final.digit <- final.digit1$count - round(final.digit1$count, digits = -1)
-final.digit[final.digit < 0] <- 10 + final.digit[final.digit < 0] 
-t2 <- data.frame(table(final.digit))
-
-p10 <- ggplot(t2, aes(x=final.digit, y=Freq)) + 
-  geom_bar(stat="identity") 
-
-plot10 <- p10 +
-  geom_bar(stat="identity", fill="grey16") +
-  xlab("Final Digit (under 5, other cases)") +
-  ylab("Frequency") +
-  ggtitle("Occurances of Final Digit (EWARN)") +
-  theme_base() +
-  theme(plot.title = element_text(size = 12, face = "bold"))+
-  theme(axis.title.x = element_text(size = 11),
-        axis.title.y = element_text(size = 11))
-
-# Plot 11
-
-count_M_18_59_other 
-
-final.digit = 0:9
-Freq = c(15, 0, 0, 2, 2, 5, 1, 3, 4, 3)
-t3 <- data.frame(cbind(final.digit, Freq))
-t3$final.digit <- as.factor(t3$final.digit)
-
-p11 <- ggplot(t3, aes(x=final.digit, y=Freq)) + 
-  geom_bar(stat="identity") 
-
-plot11 <- p11 +
-  geom_bar(stat="identity", fill="grey16") +
-  xlab("Final Digit (male 18-59, other conditions)") +
-  ylab("Frequency") +
-  ggtitle("Occurances of Final Digit (HIS)") +
-  theme_base() +
-  theme(plot.title = element_text(size = 12, face = "bold"))+
-  theme(axis.title.x = element_text(size = 11),
-        axis.title.y = element_text(size = 11))
-
-# Plot 12
-
-count_F_5_17_other
-
-final.digit = 0:9
-Freq = c(12, 1, 2, 2, 3, 2, 3, 4, 2, 4)
-t4 <- data.frame(cbind(final.digit, Freq))
-t4$final.digit <- as.factor(t4$final.digit)
-
-p12 <- ggplot(t4, aes(x=final.digit, y=Freq)) + 
-  geom_bar(stat="identity") 
-
-plot12 <- p12 +
-  geom_bar(stat="identity", fill="grey16") +
-  xlab("Final Digit (female 5-17, other conditions)") +
-  ylab("Frequency") +
-  ggtitle("Occurances of Final Digit (HIS)") +
-  theme_base() +
-  theme(plot.title = element_text(size = 12, face = "bold")) +
-  theme(axis.title.x = element_text(size = 11),
-        axis.title.y = element_text(size = 11))
-        
-
-png('//cdc.gov/private/L330/ykf1/New folder/Data Quality/Paper/digit.png', 8, 6.5, units='in', res=300)
-
-multiplot(plot11, plot12, plot9, plot10, cols=2)
-
-dev.off()
 
 
 
